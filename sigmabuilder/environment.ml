8804175ad9702a1ed99a2f7c3b625d841db15ac4
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2022-09-19 16:55:39 -0400

    merge

--- sigmabuilder/environment.ml
+++ sigmabuilder/environment.ml
@@ -1,8 +1,8 @@
-module Syn = Lambdasyn 
-open SpecLang 
+module Syn = Lambdasyn
+open SpecLang
 
 exception NoMappingForVar of string
-exception IllegalConstructorType of string    
+exception IllegalConstructorType of string
 (*A var to refinement type mapping typing environment*)
 module TypingEnv = struct
 
@@ -16,61 +16,61 @@ module TypingEnvValue =
        struct
          (*Need to change later to scehema*)
          type t = RefinementType.t
-         let equal (t1,t2) = RefinementType.compare_types t1 t2           
+         let equal (t1,t2) = RefinementType.compare_types t1 t2
 end
 
-module TyMap   = Applicativemap.ApplicativeMap (TypingEnvKey) (TypingEnvValue) 
+module TyMap   = Applicativemap.ApplicativeMap (TypingEnvKey) (TypingEnvValue)
 
 type t = TyMap.t
 let empty = TyMap.empty
 let mem = TyMap.mem
-let find t var = 
-    try (TyMap.find t var) 
-  with 
+let find t var =
+    try (TyMap.find t var)
+  with
   | (TyMap.KeyNotFound k) -> raise (NoMappingForVar k)
 
 let add = fun t -> fun var rt -> TyMap.add t var rt
 let remove = TyMap.remove
-let append t binds = 
+let append t binds =
         t@binds
 
-let toString t = 
-    List.fold_left (fun accstr (vi, rti) -> (accstr^"\n "^(Var.toString vi)^" : "^(RefTy.toString rti))) " " t 
+let toString t =
+    List.fold_left (fun accstr (vi, rti) -> (accstr^"\n "^(Var.toString vi)^" : "^(RefTy.toString rti))) " " t
 
-    
+
 
 (*enumerate variables and find macthing types*)
-let enumerateAndFind t (rt:TypingEnvValue.t) : ((TypingEnvKey.t*TypingEnvValue.t) list)   = 
-        TyMap.enumerate t rt 
+let enumerateAndFind t (rt:TypingEnvValue.t) : ((TypingEnvKey.t*TypingEnvValue.t) list)   =
+        TyMap.enumerate t rt
 
 let lambdas4RetType t (rt:TypingEnvValue.t)  : ((TypingEnvKey.t*TypingEnvValue.t) list) =
-   
+
    let rec loop gamma (filtered:(TypingEnvKey.t*TypingEnvValue.t) list) =
-        match gamma with 
+        match gamma with
             [] -> filtered
-            | (vi, rti) :: xs-> 
-                    match rti with 
-                        | RefinementType.Arrow ((arg, argty),_) -> 
-                              let uncurried =   RefinementType.uncurry_Arrow rti in 
-                                
-                            let RefinementType.Uncurried (_, retty)= RefinementType.uncurry_Arrow rti in 
-                        
-                                
-                            if (RefinementType.compare_types rt retty) then  
-                                let filtered = filtered@[(vi, rti)] in 
+            | (vi, rti) :: xs->
+                    match rti with
+                        | RefinementType.Arrow ((arg, argty),_) ->
+                              let uncurried =   RefinementType.uncurry_Arrow rti in
+
+                            let RefinementType.Uncurried (_, retty)= RefinementType.uncurry_Arrow rti in
+
+
+                            if (RefinementType.compare_types rt retty) then
+                                let filtered = filtered@[(vi, rti)] in
                                 loop xs  filtered
-                            else 
+                            else
                                 loop xs  filtered
-                        | _ ->  loop xs  filtered          
-  in 
+                        | _ ->  loop xs  filtered
+  in
  loop t []
 
 
-(*TODO :: Currently the filter directly returns the same gamma, needs updation later*)          
-let filterOnEffectSet t eff = 
-      t            
+(*TODO :: Currently the filter directly returns the same gamma, needs updation later*)
+let filterOnEffectSet t eff =
+      t
 
-end 
+end
 
 (*The constructor environment Sigma*)
 module Constructors = struct
@@ -86,41 +86,41 @@ module ConstructorEnvValue =
        struct
          (*Need to change later to scehema*)
          type t = RefinementType.t
-         let equal (t1,t2) = RefinementType.compare_types t1 t2           
+         let equal (t1,t2) = RefinementType.compare_types t1 t2
 
 end
 
-module ConsMap   = Applicativemap.ApplicativeMap (ConstructorEnvKey) (ConstructorEnvValue) 
+module ConsMap   = Applicativemap.ApplicativeMap (ConstructorEnvKey) (ConstructorEnvValue)
 
 type t = ConsMap.t
 let empty = ConsMap.empty
 let mem = ConsMap.mem
-let find t tyd = 
-    try (ConsMap.find t tyd) 
-  with 
+let find t tyd =
+    try (ConsMap.find t tyd)
+  with
    (ConsMap.KeyNotFound k) -> raise (NoMappingForVar k)
 
-let add = fun t -> fun tyd sort -> ConsMap.add t tyd sort 
+let add = fun t -> fun tyd sort -> ConsMap.add t tyd sort
 let remove = ConsMap.remove
 
-let findCons4retT t retT : ((Var.t*RefinementType.t)list ) = 
-  let rec traversal gamma acc  =      
-    (match gamma with 
+let findCons4retT t retT : ((Var.t*RefinementType.t)list ) =
+  let rec traversal gamma acc  =
+    (match gamma with
         | [] -> acc
-        | (vi, rti):: xs -> 
-                match rti with 
-                 | RefinementType.Arrow ((v,t1), t2) -> 
-                        if (RefinementType.compare_types t2 retT) then 
-                          let acc = (vi, rti) :: acc in 
-                          traversal xs acc 
+        | (vi, rti):: xs ->
+                match rti with
+                 | RefinementType.Arrow ((v,t1), t2) ->
+                        if (RefinementType.compare_types t2 retT) then
+                          let acc = (vi, rti) :: acc in
+                          traversal xs acc
                         else
                           traversal xs acc
-                 | _ -> raise (IllegalConstructorType "findCons4RetT")  
+                 | _ -> raise (IllegalConstructorType "findCons4RetT")
     )
-  in 
+  in
  traversal t empty
 
-end 
+end
 
 
 (*A map from component name a monExp*)
@@ -137,25 +137,25 @@ module ComponentEnvValue =
        struct
          (*Need to change later to scehema*)
          type t = RefinementType.t
-         let equal (t1,t2) = true           
+         let equal (t1,t2) = true
 
 end
 
-module CompMap   = Applicativemap.ApplicativeMap (ComponentEnvKey) (ComponentEnvValue) 
+module CompMap   = Applicativemap.ApplicativeMap (ComponentEnvKey) (ComponentEnvValue)
 
 type t = CompMap.t
 let empty = CompMap.empty
 let mem = CompMap.mem
-let find t name = 
-    try (CompMap.find t name) 
-  with 
+let find t name =
+    try (CompMap.find t name)
+  with
   | (CompMap.KeyNotFound k) -> raise (NoMappingForVar k)
 
-let add = fun t -> fun name comp -> CompMap.add t name comp 
+let add = fun t -> fun name comp -> CompMap.add t name comp
 let remove = CompMap.remove
 
 end
-(* 
+(*
 (*To be populated later*)
 module RelationalEnv = struct
 
@@ -169,11 +169,11 @@ module Relation =
        struct
          Need to change later to scehema
          type t = StructuralRelation.t
-         let equal (t1,t2) = true           
+         let equal (t1,t2) = true
 
 end
 
-module ConsMap   = Applicativemap.ApplicativeMap (ConstructorEnvKey) (ConstructorEnvValue) 
+module ConsMap   = Applicativemap.ApplicativeMap (ConstructorEnvKey) (ConstructorEnvValue)
 
 
 
@@ -181,40 +181,36 @@ module ConsMap   = Applicativemap.ApplicativeMap (ConstructorEnvKey) (Constructo
 
 end
  *)
-module ExploredTerms = struct 
+module ExploredTerms = struct
   type t = Var.t list
-  let equal t1 t2 = Var.equal t1 t2    
-  let empty = []  
-  let find t (e:Var.t) = List.find (fun e' -> (Var.equal e e')) t 
+  let equal t1 t2 = Var.equal t1 t2
+  let empty = []
+  let find t (e:Var.t) = List.find (fun e' -> (Var.equal e e')) t
   let add t e = e::t
   let remove t e  =
-        List.filter (fun e' -> not (Var.equal e e')) t 
+        List.filter (fun e' -> not (Var.equal e e')) t
 
-  let mem t e = List.exists (fun e' -> equal e e') t 
-let toString t = 
+  let mem t e = List.exists (fun e' -> equal e e') t
+let toString t =
         List.fold_left (fun stracc ti -> (stracc^(Var.toString ti))) "Terms" t
 end
 
 
-module ExploredPaths = struct 
+module ExploredPaths = struct
   type t = Syn.path list
-  let equal t1 t2  =  
-    try 
+  let equal t1 t2  =
+    try
         List.fold_left2  (fun accBool ci cj -> accBool && Syn.equalPath ci cj) true t1 t2
-    with 
-        Invalid_argument e-> false  
-  let empty = []  
-  let find t (e : Syn.path) = List.find (fun e' -> (Syn.equalPath e e')) t 
+    with
+        Invalid_argument e-> false
+  let empty = []
+  let find t (e : Syn.path) = List.find (fun e' -> (Syn.equalPath e e')) t
   let add t e = e::t
   let remove t e  =
-        List.filter (fun e' -> not (Syn.equalPath e e')) t 
+        List.filter (fun e' -> not (Syn.equalPath e e')) t
 
-  let mem t e = List.exists (fun ei -> Syn.equalPath e ei) t 
+  let mem t e = List.exists (fun ei -> Syn.equalPath e ei) t
   let fromList ls = ls
-  let toString t = 
+  let toString t =
         List.fold_left (fun stracc ti -> (stracc^"\n"^(Syn.pathToString ti))) "Paths" t
 end
-
-
-
-
